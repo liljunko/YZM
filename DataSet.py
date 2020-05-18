0b21757e0b9140672bc16f0a1baf325be43a1148
ChenYuyang <cyy.chen@foxmail.com>
2020-05-18 23:21:20 +0800

    done model of resnet

--- DataSet.py
+++ DataSet.py
@@ -1,17 +1,26 @@
-from torch.utils.data import dataset
-import random
+import torch
 import numpy as np
 from captcha.image import ImageCaptcha
 from tqdm import tqdm
 from torchvision.transforms.functional import to_tensor, to_pil_image
 import string
 
-characters = string.digits + string.ascii_lowercase + string.ascii_uppercase
+background_char = "-"
+characters = [
+    i
+    for i in (
+        background_char
+        + string.digits
+        + string.ascii_lowercase
+        + string.ascii_uppercase
+    )
+]
 char_pos = {c: i for i, c in enumerate(characters)}
+num_classes = len(characters)
 
-
+# [classes, background]
 def encode(chars):
-    for_ret = torch.zeros(len(characters) + 1)  # +1 是背景
+    for_ret = torch.zeros(len(characters))  # +1 是背景
     for c in chars:
         for_ret[char_pos[c]] = 1
     return for_ret
@@ -21,18 +30,18 @@ def decode(tensor):
     """
         向量转字符
     """
-    return characters[torch.argmax(tensor)]
+    pos = torch.argmax(tensor)
+    return characters[pos]
 
 
-class FakeDataset(Dataset):
+class FakeDataset(torch.utils.data.Dataset):
     def __init__(self, dataset_size):
         super(FakeDataset, self).__init__()
         self.size = dataset_size
         self.width = 128
         self.height = 64
-        self.input_length = 4
         self.label_length = 4
-        self.n_class = len(characters) + 1  # 包括背景
+        self.n_class = num_classes
         self.generator = ImageCaptcha(width=width, height=height)
 
     def __len__(self):
@@ -40,7 +49,7 @@ class FakeDataset(Dataset):
 
     def __getitem__(self, index):
         random_str = "".join(
-            [random.choice(self.characters[1:]) for j in range(self.label_length)]
+            np.random.choice(characters, self.label_length, replace=False)
         )
         image = to_tensor(self.generator.generate_image(random_str))
         target = encode(random_str)
---
+++ model.py
@@ -0,0 +1,63 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+from torchvision.models.resnet import BasicBlock
+
+model_urls = {
+    "resnet18": "https://download.pytorch.org/models/resnet18-5c106cde.pth",
+    "resnet34": "https://download.pytorch.org/models/resnet34-333f7ec4.pth",
+    "resnet50": "https://download.pytorch.org/models/resnet50-19c8e357.pth",
+    "resnet101": "https://download.pytorch.org/models/resnet101-5d3b4d8f.pth",
+    "resnet152": "https://download.pytorch.org/models/resnet152-b121ed2d.pth",
+    "resnext50_32x4d": "https://download.pytorch.org/models/resnext50_32x4d-7cdf4587.pth",
+    "resnext101_32x8d": "https://download.pytorch.org/models/resnext101_32x8d-8ba56ff5.pth",
+    "wide_resnet50_2": "https://download.pytorch.org/models/wide_resnet50_2-95faca4d.pth",
+    "wide_resnet101_2": "https://download.pytorch.org/models/wide_resnet101_2-32ee1156.pth",
+}
+
+
+class NonFcResNet(models.ResNet):
+    def __init__(
+        self, input_shape, output_lenght, block, layers, num_classes, **kwargs
+    ):
+        super(NonFcResNet, self).__init__(block, layers, num_classes, **kwargs)
+        self.avgpool = torch.nn.AdaptiveAvgPool2d(output_lenght, 1)
+
+        self.ch2cls = torch.nn.Conv2d(
+            in_channel=512 * block.expansion, out_channel=num_classes, kernel_size=1
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.relu(self.avgpool(x))
+        x = self.ch2cls(x)
+
+        # shape = [batch,channel,width]
+        x = x.squeeze(-1)
+        x = nn.functional.log_softmax(x, 1)
+        return x
+
+
+def make_model(arch, layers, num_classes, pretrained, progress=True, **kwargs):
+    model = NonFcResNet(BasicBlock, layers, num_classes, **kwargs)
+    if pretrained:
+        state_dict = models.load_state_dict_from_url(
+            model_urls[arch], progress=progress
+        )
+        model.load_state_dict(state_dict)
+    return model
+
+
+import DataSet
+
+model = make_model("resnet50", [3, 4, 6, 3], DataSet.num_classes, False)
+model(torch.ones(1, 3, 128, 64))
