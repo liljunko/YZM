037cdd177e1ad3dc130f5422cea2988e81f43fcd
ChenYuyang <cyy.chen@foxmail.com>
2020-05-21 23:05:38 +0800

    准备好训练

--- default_config.py
+++ default_config.py
@@ -1,8 +1,8 @@
 class Config:
     lr = 1e-3
-    cuda = False
-    batch_size = 4
-    num_workers = 0
+    cuda = True
+    batch_size = 256
+    num_workers = 64
     print_step = 128
     log_file = "./logs/first_logs/"
     local_rank = 0  # only for intelliense, useless otherwise
